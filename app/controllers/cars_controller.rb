f81c61dc0693c68154e0d2a5ea6aa4d4ff288a40
garuda64 <long64@hotmail.com>
2025-07-25 12:46:31 -0500

    Update cars_controller.rb

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,7 +5,8 @@ class CarsController < ApplicationController
             message: "my car",
             model: "Logan",
             fabricant: "Renault",
-            color: "Red"
+            color: "Red",
+            office: "Caribe"
         }
     }
   end
